544c0f91eb357bf6dd21220b6575b7b1a2ebf0ed
Sumud Kumar Shrivastav <sumshriv0@WKMZTE222E42.global.publicisgroupe.net>
2021-02-01 18:45:14 +0000

    two int sum in arrays

---
+++ Leetcode/src/io/arrays/solution/NumberOfOne.java
@@ -0,0 +1,22 @@
+package io.arrays.solution;
+
+public class NumberOfOne {
+
+	public static void main(String[] args) {
+
+		// you need to treat n as an unsigned value
+
+	}
+
+	public int hammingWeight(int n) {
+		String biString = Integer.toBinaryString(n);
+		int count = 0;
+		for (int i = 0; i < biString.length(); i++) {
+			if (biString.charAt(i) == '1') {
+				count++;
+			}
+		}
+		return count;
+	}
+
+}
