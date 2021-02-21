070b36942ee765df2225845faeaca17faf2bf462
Sumud <emailsumud@gmail.com>
2021-02-21 10:36:54 +0000

    Stack Implementaion with Array

---
+++ Leetcode/src/leet/algorithm/solution/StringReverseUsingStack.java
@@ -0,0 +1,23 @@
+package leet.algorithm.solution;
+
+import java.util.Stack;
+
+public class StringReverseUsingStack {
+    public static void main(String[] args) {
+        String str="abcdefg";
+        String reverse="";
+
+        Stack<Character> stack=new Stack();
+        for(int i=0;i<str.length();i++){
+            stack.push(str.charAt(i));
+        }
+        while(!stack.isEmpty())
+
+            reverse=reverse+stack.pop();
+
+        System.out.println(reverse);
+        }
+
+
+    }
+
