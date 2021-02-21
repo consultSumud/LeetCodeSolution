e78c361c81e9a0ab0d792dbf5e2669f0b83d8ce7
Sumud <emailsumud@gmail.com>
2021-02-21 10:38:57 +0000

    string Reverse with stack

--- Leetcode/src/leet/algorithm/solution/StringReverseUsingStack.java
+++ Leetcode/src/leet/algorithm/solution/StringReverseUsingStack.java
@@ -7,13 +7,13 @@ public class StringReverseUsingStack {
         String str="abcdefg";
         String reverse="";
 
-        Stack<Character> stack=new Stack();
+        Stack<Character> stack= new Stack<>();
         for(int i=0;i<str.length();i++){
             stack.push(str.charAt(i));
         }
         while(!stack.isEmpty())
 
-            reverse=reverse+stack.pop();
+            reverse=reverse.concat(stack.pop().toString());
 
         System.out.println(reverse);
         }
